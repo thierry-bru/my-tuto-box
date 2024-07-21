35a4705e83102bb8c4e3873fd98c6b9128f076f0
Thierry BRU <thierry@ARWEN>
2024-07-21 14:49:13 +0200

    configuration of stimulus, ux turbo and sass ok

--- assets/bootstrap.js
+++ assets/bootstrap.js
@@ -1,5 +1,6 @@
 import { startStimulusApp } from '@symfony/stimulus-bundle';
 
+console.log('bootstrap.js lancé')
 const app = startStimulusApp();
 // register any custom, 3rd party controllers here
 // app.register('some_controller_name', SomeImportedController);
---
+++ src/Controller/HomePageController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+class HomePageController extends AbstractController
+{
+    #[Route('/', name: 'app_home_page')]
+    public function index(): Response
+    {
+        return $this->render('home_page/index.html.twig', [
+            'controller_name' => 'HomePageController',
+        ]);
+    }
+    #[Route('/turbo/welcome', name: 'app_home_page_welcome')]
+    public function welcome(): Response
+    {
+        return $this->render('home_page/welcome.html.twig');
+    }
+}
